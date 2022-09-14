ddce7f2271a6d70001ac2c9fbdfc8cf9d28a8595
Attahiru Kamba <bappahmuhammad18@gmail.com>
2022-09-14 21:13:55 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,16 +10,14 @@
 
 int main(void)
 {
-	char *string = "_putchar";
-
-	int i;
-
-	for (i = 0; i <= 8; i++)
-	{
-		char c = string[i];
-
-		_putchar(c);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
