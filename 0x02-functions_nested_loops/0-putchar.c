8bb1292bb5114f2b1905c75939d3d06a8cf74c08
Bappa-Kamba <bappahmuhammad18@gmail.com>
2022-09-14 12:54:55 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
-/*
+/**
  * main - ENTRY POINT
+ *
+ * Description: Main code to run
  *
  * Return: Always 0
  */
+
 int main(void)
 {
 	char *string = "_putchar";
@@ -17,6 +20,5 @@ int main(void)
 
 		_putchar(c);
 	}
-	_putchar('\n');
 	return (0);
 }
