40f6f3f080da252f67e4324c716fa4a59d711c5a
Attahiru Kamba <bappahmuhammad18@gmail.com>
2022-09-14 23:50:33 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,10 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
+	if (n < 0 && n != INT_MIN)
 		n *= -1;
 	_putchar('0' + (n % 10));
-	if (n == INT_MIN)
+	if ( n < 0 && n == INT_MIN)
 	{
 		n = n % 10;
 		_putchar('0' - n);
