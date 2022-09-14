f31cf825df63385a0aca41d8c0e8a801349c5490
Attahiru Kamba <bappahmuhammad18@gmail.com>
2022-09-15 00:50:33 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,7 +14,10 @@ int main(void)
 
 	for (counter = 1; counter <= (countto / 2); counter++)
 	{
-		printf("%li, %li, ", a, b);
+		printf("%li, %li", a, b);
+		if (counter == 25)
+			continue;
+		printf(", ");
 		a += b;
 		b += a;
 	}
