4fc753091e5b838309955198f56c53275260dc9a
Attahiru Kamba <bappahmuhammad18@gmail.com>
2022-09-14 21:27:03 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include "main.h"
 
-/*
+/**
  * print_alphabet - Prints alphabets in lower case
  *
  * Description: Prints all the letters of the alphabets in lower case followed
@@ -9,22 +9,6 @@
  * Return: Alphabets in lowercase
  *
  */
-void print_alphabet(void);
-
-/*
- * main - ENTRY POINT
- *
- * Description: Main Code
- *
- * Return: Always 0
- *
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	int i = 1;
