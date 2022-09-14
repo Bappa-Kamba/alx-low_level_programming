4d821898eae664ba65b86f3c62f7c0e01badf598
Attahiru Kamba <bappahmuhammad18@gmail.com>
2022-09-14 21:17:19 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,12 @@
 int _putchar(char c);
+
+/**
+ * print_alphabet - Prints alphabets in lower case
+ *
+ * Description: Prints all the letters of the alphabets in lower case followed
+ * by a new line.
+ *
+ * Return: Alphabets in lowercase
+ *
+ */
+void print_alphabet(void);
