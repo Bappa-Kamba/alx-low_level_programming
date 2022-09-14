a34405e7818b30d21b02a5fa524c1e23074f15fa
Attahiru Kamba <bappahmuhammad18@gmail.com>
2022-09-15 00:03:13 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,13 @@
 int print_last_digit(int n)
 {
 	if (n < 0)
+	{
 		n = -(n % 10);
-
-	_putchar('0' + n);
-
+		_putchar('0' + n);
+	}
+	else
+	{
+		_putchar('0' + (n % 10);
+	}
 	return (n % 10);
 }
