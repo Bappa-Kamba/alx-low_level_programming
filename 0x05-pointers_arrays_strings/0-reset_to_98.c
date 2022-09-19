bf5534ea99f103c3815edc33bc0a2dd37b9d860b
Bappa-Kamba <bappahmuhammad18@gmail.com>
2022-09-19 10:42:20 -0700

    initital commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes a variable's value through a pointr
+ *
+ * return: Always 0
+ */
+void reset_to_98(int *p)
+{
+	*p = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ *
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
