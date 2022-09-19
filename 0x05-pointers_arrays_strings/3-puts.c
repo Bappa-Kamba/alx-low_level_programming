493b01c6a93974f3af7f3e4516eb05ee474ab58d
Bappa-Kamba <bappahmuhammad18@gmail.com>
2022-09-19 15:28:58 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - outputs to the stdout
+ * @str: string
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i >= 0; i++)
+	{
+		if (*str != '\0')
+			write(1, &str[i], 1);
+		else 
+		{
+			break;
+		}
+	}
+}
