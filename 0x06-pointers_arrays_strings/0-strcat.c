8a5df1b10669b5164b08200c76b58d96b8059d84
Bappa-Kamba <bappahmuhammad18@gmail.com>
2022-09-21 05:34:24 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcat - concatenates two strings
+*
+* @dest: string to append to
+* @src: string to  be appended
+*
+*Return: a pointer to the destination string
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++);
+	
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+	dest[i + J] = '\0';
+
+	return dest;;
+}
