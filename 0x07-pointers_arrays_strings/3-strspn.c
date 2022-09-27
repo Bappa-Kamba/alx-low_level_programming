764e3e4f691da51f7676adcb81d3c43f6dde16bc
Bappa-Kamba <bappahmuhammad18@gmail.com>
2022-09-27 07:53:11 -0700

    initil commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * _strspn - returns number of bytes in s
+  *
+  * @s: string to search
+  * @accept: string to be searched for
+  *
+  * Return: number of bytes matched
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j;
+	int matches = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				matches++;
+				break;
+			}
+			if (s[i] != accept[j] && accept[j + 1] == '\0')
+				return (matches);
+		}
+		i++;
+	}
+	return (matches);
+}
