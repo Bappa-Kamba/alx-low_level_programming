65736dcef9d54f53b60d8149403cd0cb1bcb1359
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-01-08 12:06:23 +0100

    as

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,10 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    // Creates a new hash_table_t
+    /* Creates a new hash_table_t */
     hash_table_t* table = (hash_table_t*) malloc (sizeof(hash_table_t));
     table->size = size;
-    table->arrays = (hash_node_t**) calloc (table->size, sizeof(hash_node_t*));
-    for (int i=0; i<table->size; i++)
-        table->arrays[i] = NULL;
+    table->array = (hash_node_t**) calloc (table->size, sizeof(hash_node_t*));
 
     return table;
 
