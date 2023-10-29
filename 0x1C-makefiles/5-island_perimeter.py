1ed9a29d24d49f7ab7233a3d951f0d6d9cdb92c9
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-10-29 20:21:58 +0100

    modified logic to check for water not land

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,13 +13,13 @@ def island_perimeter(grid):
             if grid[row][col] == 1:
                 perimeter += 4
 
-                if row > 0 and grid[row - 1][col] == 1:
+                if row > 0 and grid[row - 1][col] == 0:
                     perimeter -= 1  # Decrease by 2 for the top side
-                if col > 0 and grid[row][col - 1] == 1:
+                if col > 0 and grid[row][col - 1] == 0:
                     perimeter -= 1  # Decrease by 2 for the left side
-                if row < rows - 1 and grid[row + 1][col] == 1:
+                if row < rows - 1 and grid[row + 1][col] == 0:
                     perimeter -= 1  # Decrease by 2 for the bottom side
-                if col < cols - 1 and grid[row][col + 1] == 1:
+                if col < cols - 1 and grid[row][col + 1] == 0:
                     perimeter -= 1  # Decrease by 2 for the right side
 
     return perimeter
