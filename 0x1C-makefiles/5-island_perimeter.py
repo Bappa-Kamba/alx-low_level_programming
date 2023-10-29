794cfab1ea90b9a09fbf172d231293c7922fdd9f
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-10-29 15:21:55 +0100

    commit

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Island - Perimeter function
+"""
+
+
+def island_perimeter(grid):
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1  # Decrease by 2 for the top side
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 1  # Decrease by 2 for the left side
+                if row < rows - 1 and grid[row + 1][col] == 1:
+                    perimeter -= 1  # Decrease by 2 for the bottom side
+                if col < cols - 1 and grid[row][col + 1] == 1:
+                    perimeter -= 1  # Decrease by 2 for the right side
+
+    return perimeter
